7bc31f6e27edf1276dbe1bca1d64d0807f9eaabc
Kayo Leanndro <kayo.leanndro@gruporas.com.br>
2024-05-08 13:54:05 -0300

    Correção erro imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,18 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+from flask.cli import FlaskGroup
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}?client_encoding=utf8"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
 
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
-
+cli = FlaskGroup(app)
 
 from app.controllers import default
+
+if __name__ == "__main__":
+    cli()
